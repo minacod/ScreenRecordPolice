d8a23c6577e1addef326b5622f2f96d8e591a446
Mina Abdelfady <mina.abdelfady@coformatique.com>
2020-07-16 09:36:49 +0200

    AVPlayer added

--- ScreenRecordPolice/ViewController.swift
+++ ScreenRecordPolice/ViewController.swift
@@ -7,21 +7,38 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var VideoView: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         NotificationCenter.default.addObserver(self, selector: #selector(preventScreenRecording), name: UIScreen.capturedDidChangeNotification, object: nil)
-        
+        VideoView.setTitle("Click To Watch", for: .normal)
+        VideoView.addTarget(self, action: #selector(presentAV), for: .touchDown)
     }
 
+    @objc func presentAV() {
+        let url = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!
+        let player = AVPlayer(url: url)
+        
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        playerViewController.setValue(true, forKey: "requiresLinearPlayback")
+        present(playerViewController, animated: true) {
+          player.play()
+        }
+    }
+    
     @objc func preventScreenRecording() {
         label.text = "You have Been Caught Video Stoped"
+        VideoView.isEnabled = false
+        navigationController?.dismiss(animated: true, completion: nil)
     }
 
 }
